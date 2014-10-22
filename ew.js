ce52784037ff77c04f4dc37bb34404b022a610d6
hioggia <mikokuu@gmail.com>
2014-10-22 11:11:22 +0800

    UPDATE

--- ew.js
+++ ew.js
@@ -1,9 +1,9 @@
 var MAX_RESTAURANT_NAME_LENGTH = 16;
 
-var dataAccess = (function(){
+var dataAccessFactory = function(key){
 	var _data = [];
-	if(localStorage['restaurants']){
-		_data = JSON.parse(localStorage['restaurants']);
+	if(localStorage[key]){
+		_data = JSON.parse(localStorage[key]);
 	}
 
 	return {
@@ -16,8 +16,12 @@ var dataAccess = (function(){
 			_data.splice(index,1);
 			this.save();
 		},
+		clear: function(){
+			_data = [];
+			this.save();
+		},
 		save: function(){
-			localStorage['restaurants'] = JSON.stringify(_data);
+			localStorage[key] = JSON.stringify(_data);
 		},
 		get: function(index){
 			return _data[index];
@@ -29,7 +33,10 @@ var dataAccess = (function(){
 			return _data.length;
 		}
 	}
-})();
+};
+
+var dataRestaurants = dataAccessFactory('restaurants');
+var dataRecord = dataAccessFactory('record');
 
 var el = function(selector){
 	return document.querySelector(selector);
@@ -65,7 +72,7 @@ var eventHandler = {
 			eventHandler.addRestaurant(name);
 			return;
 		}
-		dataAccess.add(name);
+		dataRestaurants.add(name);
 		eventHandler.refreshCounter();
 	},
 	sceneJump: function(sceneId){
@@ -73,10 +80,10 @@ var eventHandler = {
 		addClass(el(sceneId),'curr_scene');
 	},
 	gotoManager: function(){
-		var l = dataAccess.size(), r = [];
+		var l = dataRestaurants.size(), r = [];
 		var template = el('[rel="manage_item"]').innerHTML;
 		for(var i=0;i<l;i++){
-			r.push(template.replace(/\{index\}/g,i).replace(/\{name\}/g,dataAccess.get(i)));
+			r.push(template.replace(/\{index\}/g,i).replace(/\{name\}/g,dataRestaurants.get(i)));
 		}
 		el('[rel="manage_list"]').innerHTML = r.join('');
 		eventHandler.sceneJump('#manage');
@@ -85,12 +92,12 @@ var eventHandler = {
 		eventHandler.sceneJump('#main');
 	},
 	getDailyRestaurant: function(){
-		if(dataAccess.size()==0){
+		if(dataRestaurants.size()==0){
 			alert('你还没有添加过任何吃饭场所喔');
 			return;
 		}
-		var r = Math.floor(dataAccess.size()*Math.random());
-		el('[rel="dialy_title"]').innerText = dataAccess.get(r);
+		var r = Math.floor(dataRestaurants.size()*Math.random());
+		el('[rel="dialy_title"]').innerText = dataRestaurants.get(r);
 		eventHandler.sceneJump('#daily');
 	},
 	getWeeklyRestaurant: function(){
@@ -99,20 +106,36 @@ var eventHandler = {
 			alert('这周的天数好像？嗯？我在地球吗？');
 			return;
 		}
-		if(dataAccess.size()==0){
+		if(dataRestaurants.size()==0){
 			alert('你还没有添加过任何吃饭场所喔');
 			return;
 		}
+		dataRecord.clear();
 		var a = [], r = [];
 		while(d>a.length){
-			a = a.concat(dataAccess.all());
+			a = a.concat(dataRestaurants.all());
 		}
 		var template = el('[rel="weekly_cell"]').innerHTML;
 		for(var i=0;i<d;i++){
 			var idx = Math.floor(a.length*Math.random());
 			var name = a[idx];
+			dataRecord.add(name);
 			r.push(template.replace(/\{date\}/g,i+1).replace(/\{name\}/g,name));
 			a.splice(idx,1);
+		}
+		el('[rel="weekly_table"]').innerHTML = r.join('');
+		eventHandler.sceneJump('#weekly');
+	},
+	showWeeklyRestaurant: function(){
+		if(dataRecord.size()==0){
+			alert('现在没有记录');
+			return;
+		}
+		var r = [];
+		var template = el('[rel="weekly_cell"]').innerHTML;
+		for(var i=0;i<dataRecord.size();i++){
+			var name = dataRecord.get(i);
+			r.push(template.replace(/\{date\}/g,i+1).replace(/\{name\}/g,name));
 		}
 		el('[rel="weekly_table"]').innerHTML = r.join('');
 		eventHandler.sceneJump('#weekly');
@@ -121,13 +144,13 @@ var eventHandler = {
 		var selectedItems = els('[rel="list_item"]:checked');
 		for(var i=selectedItems.length-1;i>=0;i--){
 			var idx = ~~selectedItems[i].value;
-			dataAccess.remove(idx);
+			dataRestaurants.remove(idx);
 		}
 		eventHandler.refreshCounter();
 		eventHandler.gotoManager();
 	},
 	refreshCounter: function(){
-		el('[rel="restaurant_count"]').innerText = dataAccess.size();
+		el('[rel="restaurant_count"]').innerText = dataRestaurants.size();
 	}
 };
 
@@ -142,6 +165,8 @@ el('[rel="add_new_restaurant"]').addEventListener('click',eventHandler.addRestau
 el('[rel="create_menu_daily"]').addEventListener('click',eventHandler.getDailyRestaurant,false);
 
 el('[rel="create_menu_weekly"]').addEventListener('click',eventHandler.getWeeklyRestaurant,false);
+
+el('[rel="look_last_weekly_menu"]').addEventListener('click',eventHandler.showWeeklyRestaurant,false);
 
 el('[rel="modify_exists_restaurants"]').addEventListener('click',eventHandler.gotoManager,false);
 
