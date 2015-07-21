79bbaeed0fdf427209d44e4d87e3461f72d34b24
hioggia <mikokuu@gmail.com>
2015-07-21 10:00:45 +0800

    update

--- coopraid_offer.js
+++ coopraid_offer.js
@@ -1,21 +1,26 @@
 function _tapJoin(){$('.btn-usual-join:visible').trigger('tap');$('.btn-usual-ok:visible').trigger('tap');_autoTapJoin()}
-function _autoTapJoin(){setTimeout(_tapJoin,50)}
+function _autoTapJoin(){if(/coopraid\/offer/i.test(location.hash))setTimeout(_tapJoin,50)}
 _autoTapJoin();
 //$('.prt-wanted-list').on('tap',autoTapJoin);
 console.info('单击参加已启用。你还可以输入 autoFindRoom(\'h8|Ｈ8|ハード8\') 自动加入房间。')
 
-var _keyword = /サジ|匙|ex3\-3|ex3/i;
+var _keyword = /サジ|匙|ex3\-3|ex3/i,_joinPandemo=false;
 function _findAndTapSaji(){_autoSaji();$('.prt-wanted-room').each(function(i,el){
 	var t=$('.txt-room-comment',el).text();
-	if(_keyword.test(t) && $('.prt-member-image',el).children().size()<3){
+	if(_joinPandemo && $('.prt-invite-type-4',el).size()>0 && $('.prt-member-image',el).children().size()<3){
+		$(el).trigger('tap');console.log(t)
+	}
+	if(_keyword.test(t) && $('.prt-member-image',el).children().size()<3 && $('.prt-invite-type-2,.prt-invite-type-3',el).size()==0){
 		$(el).trigger('tap');console.log(t)
 	}
 })}
 function _autoSaji(){setTimeout(_findAndTapSaji,50)}
-function autoFindRoom(comment){
+function autoFindRoom(comment,joinPandemo){
+	_joinPandemo = !!joinPandemo;
 	_keyword = new RegExp(comment,'i');
 	_autoSaji();
 	_autoRenew();
+	//return '输入 stop 来停止。'
 }
 
 var _waitSec = 3;
--- raid_helper.js
+++ raid_helper.js
@@ -2,5 +2,30 @@ function setHp(index){$('.btn-enemy-gauge[target='+ (index+1)+']').html('<span c
 function setHpAll(){for(var l=stage.pJsnData.boss.param.length;l>0;l--){setHp(l-1)}}
 function renewHpAll(){setHpAll();setTimeout(renewHpAll,1000);}
 $('<style>.wg_hpshow{position:absolute;color:#f2eee2;text-shadow:0 0 1px #0c320d,0 0 1px #0c320d,0 0 1px #0c320d,0 0 1px #0c320d,0 0 2px #0c320d,0 0 2px #0c320d,0 0 2px #0c320d,0 0 2px #0c320d;font-size:0.7em;bottom:-5px;right:5px;}</style>').appendTo(document.body);
-setTimeout(renewHpAll,3000);
-console.info('血量显示已启用。')
+
+//var m_bghf = require('lib/raid/display').mBossGaugeHpForLog;
+require('lib/raid/display').mBossGaugeHp = function(a,b,c,d){
+	a.call(function(a, b, c) {
+        require('lib/raid/display').mBossGaugeHpForLog(a, b, c);
+		setHpAll();
+    },
+    [b, c, d]);
+};
+
+setHpAll();
+console.info('血量显示已启用。');
+
+function flashBattle(){
+	if(require && require.specified('lib/raid/motion')){
+		require('lib/raid/motion').mWaitAll = function(a, b) {
+            b.playtime = 0;
+            for (var c = 0; c <= a.length - 1; c++) for (var d = 0; d <= a[c].timeline.length - 1; d++) a[c].timeline[d].wait(b.playtime);
+            return ! 0
+        };
+        console.info('闪电战术已启用。');
+	}else{
+		setTimeout(flashBattle,1000);
+	}
+}
+
+flashBattle();
