1528ca3bc2a778716a19ea28c18f4ce6469a7de9
hioggia <mikokuu@gmail.com>
2015-07-28 01:02:54 +0800

    app support

--- main_controller.js
+++ main_controller.js
@@ -1,4 +1,5 @@
 var host = 'http://hioggia.github.io/eatWhat/';
+var inspected = false;
 
 delete window.onerror;
 delete console.log;
@@ -16,6 +17,7 @@ Game.reportError = function(msg, url, line, column, err, callback){console.log(m
 var createScriptLoader = function(file,readySerif){
 	console.log('loading '+file+' ...');
 	var s = document.createElement('script');
+	s.onload = function(){inspected=true};
 	if(readySerif==undefined){readySerif='别急，很快就要开始了。'}
 	var t = "function mp(){\
 		var s=document.createElement('script');\
@@ -35,30 +37,38 @@ var createScriptLoader = function(file,readySerif){
 	document.body.appendChild(s);
 };
 
-if(/casino\/game\/slot/i.test(location.hash)){
-	createScriptLoader('casino_slot.js?v=1');
-}
+var inspector = function(){
+	if(inspected){
+		return;
+	}
+	
+	if(/casino\/game\/slot/i.test(location.hash)){
+		createScriptLoader('casino_slot.js?v=1');
+	}
 
-else if(/casino\/game\/poker/i.test(location.hash)){
-	createScriptLoader('casino_poker.js?v=1');
-}
+	else if(/casino\/game\/poker/i.test(location.hash)){
+		createScriptLoader('casino_poker.js?v=1');
+	}
 
-else if(/casino\/game\/bingo/i.test(location.hash)){
-	createScriptLoader('casino_bingo.js?v=1','请稍后。');
-}
+	else if(/casino\/game\/bingo/i.test(location.hash)){
+		createScriptLoader('casino_bingo.js?v=1','请稍后。');
+	}
 
-else if(/event\/teamraid\d+\/ranking_guild\/detail/i.test(location.hash) || /event\/teamraid\d+\/ranking\/detail/i.test(location.hash)){
-	createScriptLoader('teamraid_ranker.js?v=1','请稍后。');
-}
+	else if(/event\/teamraid\d+\/ranking_guild\/detail/i.test(location.hash) || /event\/teamraid\d+\/ranking\/detail/i.test(location.hash)){
+		createScriptLoader('teamraid_ranker.js?v=1','请稍后。');
+	}
 
-else if(/raid\/\d+/i.test(location.hash) || /raid_multi\/\d+/i.test(location.hash)){
-	createScriptLoader('raid_helper.js?v=2','请稍后。');
-}
+	else if(/raid\/\d+/i.test(location.hash) || /raid_multi\/\d+/i.test(location.hash)){
+		createScriptLoader('raid_helper.js?v=2','请稍后。');
+	}
 
-else if(/coopraid\/offer/i.test(location.hash)){
-	createScriptLoader('coopraid_offer.js?v=2','请稍后。');
-}
+	else if(/coopraid\/offer/i.test(location.hash)){
+		createScriptLoader('coopraid_offer.js?v=2','请稍后。');
+	}
 
-else if(/quest\/assist/i.test(location.hash)){
-	createScriptLoader('quest_assist.js?v=1','请稍后。');
-}
+	else if(/quest\/assist/i.test(location.hash)){
+		createScriptLoader('quest_assist.js?v=1','请稍后。');
+	}
+};
+
+inspector();
