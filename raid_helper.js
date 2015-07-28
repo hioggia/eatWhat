006cfb196dcd6aa422da6b96b66f07e7f77bf9a6
hioggia <mikokuu@gmail.com>
2015-07-28 15:45:23 +0800

    update

--- raid_helper.js
+++ raid_helper.js
@@ -1,10 +1,10 @@
 function setHp(index){$('.btn-enemy-gauge[target='+ (index+1)+']').html('<span class="wg_hpshow">'+stage.pJsnData.boss.param[index].hp+'/'+stage.pJsnData.boss.param[index].hpmax)}
-function setHpAll(){for(var l=stage.pJsnData.boss.param.length;l>0;l--){setHp(l-1)}}
-function renewHpAll(){setHpAll();setTimeout(renewHpAll,1000);}
-$('<style>.wg_hpshow{position:absolute;color:#f2eee2;text-shadow:0 0 1px #0c320d,0 0 1px #0c320d,0 0 1px #0c320d,0 0 1px #0c320d,0 0 2px #0c320d,0 0 2px #0c320d,0 0 2px #0c320d,0 0 2px #0c320d;font-size:0.7em;bottom:-5px;right:5px;}</style>').appendTo(document.body);
+function setHpAll(){if(window.stage &&stage.pJsnData){for(var l=stage.pJsnData.boss.param.length;l>0;l--){setHp(l-1)}}else{setTimeout(setHpAll,500)}}
+function setStyle(){$('<style>.wg_hpshow{position:absolute;color:#f2eee2;text-shadow:0 0 1px #0c320d,0 0 1px #0c320d,0 0 1px #0c320d,0 0 1px #0c320d,0 0 2px #0c320d,0 0 2px #0c320d,0 0 2px #0c320d,0 0 2px #0c320d;font-size:0.7em;bottom:-5px;right:5px;}</style>').appendTo(document.body);}
 
 //var m_bghf = require('lib/raid/display').mBossGaugeHpForLog;
 function hpvis(){
+	//try{
 	if(require && require.specified('lib/raid/display')){
 		require('lib/raid/display').mBossGaugeHp = function(a,b,c,d){
 			a.call(function(a, b, c) {
@@ -14,17 +14,22 @@ function hpvis(){
 		    [b, c, d]);
 		};
 
+		setStyle();
 		setHpAll();
 		console.info('血量显示已启用。');
 	}else{
 		setTimeout(hpvis,1000);
 	}
+	//}catch(ex){
+	//	console.log('hpvis',ex);
+	//}
 }
 
 function blitz(){
 	if(require && require.specified('lib/raid/motion')){
 		require('lib/raid/motion').mWaitAll = function(a, b) {
-            b.playtime = 0;
+			b.playtime = (b.playtime || 10)/5;
+            //b.playtime = 0;
             for (var c = 0; c <= a.length - 1; c++) for (var d = 0; d <= a[c].timeline.length - 1; d++) a[c].timeline[d].wait(b.playtime);
             return ! 0
         };
@@ -35,17 +40,23 @@ function blitz(){
 }
 
 function appendBtn(){
+	//try{
 	if($ && $('.btn-attack-start').size()>0){
-		var cmd = $('<button style="position:absolute;right:10px;bottom:5px;z-index:5;">Blitz!</button>').appendTo('.btn-attack-start');
+		var cmd = $('<button style="position:absolute;top:0;z-index:5;width:45px;height:22px">Blitz!</button>').appendTo(document.body);
 		cmd.on('tap',function(){
 			cmd.off('tap');
 			cmd.remove();
-			blitz();
+			if($('.btn-attack-start').size()>0){
+				blitz();
+			}
 			//$('.btn-attack-start').trigger('tap');
 		});
 	}else{
 		setTimeout(appendBtn,500);
 	}
+	//}catch(ex){
+	//	console.log('appendBtn',ex);
+	//}
 }
 
 hpvis();
