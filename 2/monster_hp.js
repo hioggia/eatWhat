360b47ddb8db5ee0367d661d8c3f1cc3d7423c6f
hioggia <mikokuu@gmail.com>
2016-01-14 17:44:57 +0800

    percent

--- 2/monster_hp.js
+++ 2/monster_hp.js
@@ -1,10 +1,36 @@
 (function(){
 
-	function setHp(index){$('.btn-enemy-gauge[target='+ (index+1)+']').html('<span class="wg_hpshow">'+stage.pJsnData.boss.param[index].hp+'/'+stage.pJsnData.boss.param[index].hpmax)}
-	function setHpAll(){if(window.stage &&stage.pJsnData){for(var l=stage.pJsnData.boss.param.length;l>0;l--){setHp(l-1)}}else{setTimeout(setHpAll,500)}}
-	function setStyle(){$('<style>[class^="lis-character"] .txt-hp-value.hide-hp::after{display:none!important;background:none!important}.wg_hpshow{position:absolute;color:#f2eee2;text-shadow:0 0 1px #0c320d,0 0 1px #0c320d,0 0 1px #0c320d,0 0 1px #0c320d,0 0 2px #0c320d,0 0 2px #0c320d,0 0 2px #0c320d,0 0 2px #0c320d;font-size:0.7em;bottom:-16px;right:5px;}</style>').appendTo(document.body);}
+	function setHp(index){
+		$('.wg-hp-area div[target='+ (index+1)+'] span').html(
+				stage.pJsnData.boss.param[index].hp+'/'+stage.pJsnData.boss.param[index].hpmax+'<br />'
+				+Math.round(stage.pJsnData.boss.param[index].hp/stage.pJsnData.boss.param[index].hpmax*10000)/100+'%'
+			)
+	}
+	function setHpAll(){
+		if(window.stage &&stage.pJsnData){
+			for(var l=stage.pJsnData.boss.param.length;l>0;l--){
+				setHp(l-1)
+			}
+		}else{
+			setTimeout(setHpAll,500)
+		}
+	}
 
-	setStyle();
+	$('<div class="wg-hp-area"><div class="hp-show" target="1"><span></span></div><div class="hp-show" target="2"><span></span></div><div class="hp-show" target="3"><span></span></div></div>').appendTo('.prt-targeting-area');
+	$('<style>[class^="lis-character"] .txt-hp-value.hide-hp::after{display:none!important;background:none!important}\
+		.prt-targeting-area .wg-hp-area{position:absolute;top:0px;left:0px;width:320px;height:1px;pointer-events:none;}\
+		.prt-targeting-area[type="s1"] .wg-hp-area div[target="1"],.prt-targeting-area[type="m1"] .wg-hp-area div[target="1"],.prt-targeting-area[type="l1"] .wg-hp-area div[target="1"]{position:absolute;top:42px;left:30px;width:260px;height:40px}\
+		.prt-targeting-area[type="s2"] .wg-hp-area div[target="1"],.prt-targeting-area[type="m2"] .wg-hp-area div[target="1"]{position:absolute;top:47px;left:33px;width:110px;height:40px}\
+		.prt-targeting-area[type="s2"] .wg-hp-area div[target="2"],.prt-targeting-area[type="m2"] .wg-hp-area div[target="2"]{position:absolute;top:47px;left:172px;width:110px;height:40px}\
+		.prt-targeting-area[type="s3"] .wg-hp-area div[target="1"],.prt-targeting-area[type="m3"] .wg-hp-area div[target="1"]{position:absolute;top:47px;left:0px;width:106px;height:40px}\
+		.prt-targeting-area[type="s3"] .wg-hp-area div[target="2"],.prt-targeting-area[type="m3"] .wg-hp-area div[target="2"]{position:absolute;top:47px;left:214px;width:106px;height:40px}\
+		.prt-targeting-area[type="s3"] .wg-hp-area div[target="3"],.prt-targeting-area[type="m3"] .wg-hp-area div[target="3"]{position:absolute;top:47px;left:109px;width:106px;height:40px}\
+		.prt-targeting-area[type="l2"] .wg-hp-area div[target="1"]{position:absolute;top:47px;left:33px;width:110px;height:40px}\
+		.prt-targeting-area[type="l2"] .wg-hp-area div[target="2"]{position:absolute;top:47px;left:172px;width:110px;height:40px}\
+		.prt-targeting-area[type="l3"] .wg-hp-area div[target="1"]{position:absolute;top:47px;left:0px;width:106px;height:40px}\
+		.prt-targeting-area[type="l3"] .wg-hp-area div[target="2"]{position:absolute;top:47px;left:214px;width:106px;height:40px}\
+		.prt-targeting-area[type="l3"] .wg-hp-area div[target="3"]{position:absolute;top:47px;left:109px;width:106px;height:40px}\
+		.hp-show span{position:absolute;color:#f2eee2;text-shadow:0 0 1px #0c320d,0 0 1px #0c320d,0 0 1px #0c320d,0 0 1px #0c320d,0 0 2px #0c320d,0 0 2px #0c320d,0 0 2px #0c320d,0 0 2px #0c320d;font-size:0.7em;bottom:-16px;right:5px;}</style>').appendTo(document.body);
 	(function hpvis(){
 		if(require && require.specified('lib/raid/display')){
 			var display = require('lib/raid/display');
@@ -21,6 +47,18 @@
 				setHpAll();
 				return result;
 			};
+			/*var hookBossGaugeRecast = display.mBossGaugeRecast;
+			display.mBossGaugeRecast = function(a,b,c,d){
+				var result = hookBossGaugeRecast(a,b,c,d);
+				//console.log('mBossGaugeRecast:',a,b,c,d);
+				return result;
+			};
+			var hookBossGaugeRecastForLog = display.mBossGaugeRecastForLog;
+			display.mBossGaugeRecastForLog = function(a,b,c){
+				var result = hookBossGaugeRecastForLog(a,b,c);
+				//console.log('mBossGaugeRecastForLog:',a,b,c);
+				return result;
+			};*/
 
 			setHpAll();
 			console.info('血量显示已启用。');
